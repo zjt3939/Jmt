e37929a861b3fe9e0cd2aa7d61e935739951575c
zjt3939 <zjt3939@163.com>
2017-03-10 18:04:25 +0800

    test

---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_NAME' => 'thinkcmf',
+    'DB_USER' => 'root',
+    'DB_PWD' => '00000000',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'Ly_',
+    //密钥
+    "AUTHCODE" => '4m0HBrN23lUJAAExFs',
+    //cookies
+    "COOKIE_PREFIX" => 'R9Lrj5_',
+);
+?>
